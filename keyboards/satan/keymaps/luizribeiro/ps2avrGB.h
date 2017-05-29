63669144282f116f1f9c6c131edbe1c3cd14d70c
Luiz Ribeiro <luizribeiro@gmail.com>
2017-05-28 23:09:48 -0400

    Fixed satan keymap

--- keyboards/satan/keymaps/luizribeiro/ps2avrGB.h
+++ keyboards/satan/keymaps/luizribeiro/ps2avrGB.h
@@ -1,7 +1,7 @@
 // this converts from my ps2avrGB keymap to gh60 satan, so I can reuse the
 // keymap from my larger board to my smaller one
-#undef KEYMAP
-#define KEYMAP( \
+#undef KC_KEYMAP
+#define KC_KEYMAP( \
     K05, K25, K35, K45, K55, K06, KA6, KA7, K07, KB5, KC5, KD5, KE5, KD1, KE1, KE2, \
     K04, K14, K24, K34, K44, K54, K16, KB6, KB7, K17, KA4, KB4, KC4, KE4,      KD0, \
     K03, K13, K23, K33, K43, K53, K26, KC6, KC7, K27, KA3, KB3, KC3, KD3,      K67, \
