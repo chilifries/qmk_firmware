28c3fed63f17f8c9069cd73ed233c68143295007
yum <yum@rubiconproject.com>
2018-03-05 16:27:29 -0800

    Cleaned up the Iris and Gherkin

--- keyboards/gherkin/keymaps/chilifries/keymap.c
+++ keyboards/gherkin/keymaps/chilifries/keymap.c
@@ -4,16 +4,15 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	KEYMAP(
-		KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
-		KC_A,  KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ESC,
-		LCTL(KC_Z), LALT(KC_X), LT(3, KC_C), LT(4, KC_V), LT(2, KC_BSPC),   LT(1, KC_SPC), LT(5, KC_B), RALT(KC_N), RCTL(KC_M), RSFT(KC_ENT)),
+		KC_Q, KC_W, KC_F, KC_P, KC_G,   KC_J, KC_L, KC_U, KC_Y, KC_SCLN,
+		KC_A, KC_R, KC_S, KC_T, KC_D,   KC_H, KC_N, KC_E, KC_I, KC_O,
+		LSFT(KC_Z), LALT(KC_X), LT(3, KC_C), LT(4, KC_V), LT(2, KC_BSPC),
+		    LT(1, KC_SPC), LT(5, KC_B), RALT(KC_N), RCTL(KC_M), RSFT(KC_ENT)),
 
 	KEYMAP(
-		KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, 
-		KC_7, KC_8, KC_9, KC_0, KC_F1, KC_F2, 
-		KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, 
-		KC_F9, KC_F10, KC_NO, KC_NO, KC_NO, KC_NO, 
-		KC_DEL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
+		KC_1, KC_2, KC_3, KC_4, KC_5,        KC_6, KC_7, KC_8, KC_9, KC_0,
+		KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,   KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
+		KC_NO, KC_NO, KC_NO, KC_NO, KC_DEL,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
 
 	KEYMAP(
 		KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, 
