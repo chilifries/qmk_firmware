b5743fc3b18d2325fd649f2917bd046e421f10ca
yum <yum@rubiconproject.com>
2018-03-06 12:25:50 -0800

    changed default back to qwerty

--- keyboards/lets_split/keymaps/chilifries/keymap.c
+++ keyboards/lets_split/keymaps/chilifries/keymap.c
@@ -94,9 +94,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `------------------------------------------------------------------------------------'
  */
 [_COLEMAK] = KEYMAP( \
-  KC_TAB,  KC_Q,    KC_W,    KC_F,        KC_P,    KC_G,          KC_J,          KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC, \
-  CTL_ESC, KC_A,    KC_R,    LT_MC(KC_S), KC_T,    KC_D,          KC_H,          KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
-  SFT_BSP, KC_Z,    KC_X,    KC_C,        KC_V,    KC_B,          KC_K,          KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT , \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,        KC_R,    KC_T,          KC_Y,          KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS, \
+  KC_LCTL, KC_A,    KC_S,    LT_MC(KC_D), KC_F,    KC_G,          KC_H,          KC_J,    KC_K,    KC_L,    KC_SCLN,    KC_QUOT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,        KC_V,    KC_B,          KC_N,          KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
   KC_GRV,  KC_LCTL, KC_LALT, KC_LGUI,     LOWER,   LT_TC(KC_SPC), LT_TC(KC_ENT), RAISE,   KC_LGUI, MY_PREV_TAB, MY_NEXT_TAB, MY_CMD_BACK \
 ),
 
